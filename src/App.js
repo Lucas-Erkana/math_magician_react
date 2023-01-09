c364b21ba86c6d6d09942b17a5c2af29cb24a636
Lucas David Erkana <lderkana21x@gmail.com>
2023-01-09 17:59:55 +0200

    Import calculator.js to App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
+import Calculator from './components/Calculator';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.PureComponent {
+  render() {
+    return (
+      <div className="Application">
+        <div className="answer">0</div>
+        <Calculator />
+      </div>
+    );
+  }
+}
+
+export default App;
+import React from 'react';
+import Calculator from './components/Calculator';
+import './App.css';
+
+class App extends React.PureComponent {
+  render() {
+    return (
+      <div className="Application">
+        <div className="answer">0</div>
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
