a900ed0e2ed71909a9b725f223d174051604ec7b
Lucas David Erkana <lderkana21x@gmail.com>
2023-01-15 22:04:27 +0200

    import index.css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+/* eslint-disable */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+      <App />
+  </React.StrictMode>
 );
