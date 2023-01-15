b9e6995724787f39f04f033b7d2d0f7c7a210d45
Lucas David Erkana <lderkana21x@gmail.com>
2023-01-15 22:02:29 +0200

    Update App.js to display Home,Quote and Calculator

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React from 'react';
+import {
+  BrowserRouter as Router, Routes, Route, Link,
+} from 'react-router-dom';
 import Calculator from './components/Calculator';
-import './App.css';
+import Home from './components/Home';
+import Quote from './components/Quote';
+import styles from './App.module.css';
 
 class App extends React.PureComponent {
   render() {
     return (
-      <div className="Application">
-        <Calculator />
-      </div>
+      <Router>
+        <div className={styles.Application}>
+          <nav>
+            <h1 className={styles.header}>Math Magicians</h1>
+            <ul className={styles.links}>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              |
+              <li>
+                <Link to="/Calculator">Calculator</Link>
+              </li>
+              |
+              <li>
+                <Link to="/Quote">Quote</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Calculator" element={<Calculator />} />
+            <Route path="/Quote" element={<Quote />} />
+          </Routes>
+        </div>
+      </Router>
     );
   }
 }
